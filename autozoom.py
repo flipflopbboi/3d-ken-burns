6b9fabf1758f489bf95e9e7b94f0ae0702b0d3be
flipflopbboi <flipflopbboideep@gmail.com>
2020-04-01 14:50:24 +0100

    change border type

--- autozoom.py
+++ autozoom.py
@@ -314,7 +314,7 @@ if __name__ == "__main__":
             bottom=bottom,
             left=left,
             right=right,
-            borderType=cv2.BORDER_REFLECT,
+            borderType=cv2.BORDER_REPLICATE,
             value=[0, 0, 0],
         )
         bordered_frames.append(bordered_frame)
