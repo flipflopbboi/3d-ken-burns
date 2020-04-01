6aba76bb4fecd38b9a4a2350278a5593732ff851
flipflopbboi <flipflopbboideep@gmail.com>
2020-04-01 20:17:46 +0100

    restore original

--- autozoom.py
+++ autozoom.py
@@ -298,8 +298,8 @@ if __name__ == "__main__":
         objFrom = {
             "fltCenterU": args.width if args.width is not None else intWidth / 2.0,
             "fltCenterV": args.height if args.height is not None else intHeight / 2.0,
-            "intCropWidth": int(math.floor(0.5 * intWidth)),
-            "intCropHeight": int(math.floor(0.5 * intHeight)),
+            "intCropWidth": int(math.floor(0.97 * intWidth)),
+            "intCropHeight": int(math.floor(0.97 * intHeight)),
         }
 
         objTo = process_autozoom(
